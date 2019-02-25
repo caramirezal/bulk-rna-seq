3169841696f9043405dc351ac52dc683b2f2f22f
caramirezal <cramireza@ciencias.unam.mx>
2019-02-25 15:00:40 -0600

    multiqc reports from fastqc quality check

--- fastq_qc.bash
+++ fastq_qc.bash
@@ -1,11 +1,21 @@
 export PATH=$PATH:sc/FastQC
 
-## setting path where fastq files are stored
-path2fastq=/Volumes/Untitled/ABRAHAN-106648929/FASTQ_Generation_2018-12-10_17_23_26Z-142394763/
-echo $path2fastq 
- 
-fnames=$(find $path2fastq -type f)
-for i in $fnames; do
-     fastqc $i -o reports/FastQC
-done
+####################################################################################################
+## Fastqc quality check
 
+## setting path where fastq files are stored
+#path2fastq=/Volumes/Untitled/ABRAHAN-106648929/FASTQ_Generation_2018-12-10_17_23_26Z-142394763/
+#echo $path2fastq 
+ 
+#fnames=$(find $path2fastq -type f)
+#for i in $fnames; do
+#     fastqc $i -o reports/FastQC
+#done
+
+##################################################################################################
+## alignments quality check
+path2bam=/Volumes/Untitled/
+echo $path2bam
+
+fastqc /Volumes/Untitled/*.bam -o reports/FastQC -t 64 
+ 
