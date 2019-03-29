82e29e8f1d6d4f99d420f6f24f61ee16dd406bcc
caramirezal <cramireza@ciencias.unam.mx>
2019-03-28 18:33:09 -0600

    add hystogram

--- data_analysis.R
+++ data_analysis.R
@@ -14,8 +14,18 @@ pheno_data$Muestra
 pheno_data <- mutate(pheno_data, Muestra=gsub(" |-|\\(|\\)", "", Muestra))
 names(pheno_data)[1] <- "ids"
 pheno_data$ids
-pheno_data$ids <- paste0(pheno_data$ids, ".ballgown.gtf", sep ="")
+pheno_data$ids <- paste0("cnss", pheno_data$ids, sep ="")
 pheno_data <- as.data.frame(pheno_data)
+pheno_data <- pheno_data[order(pheno_data$ids),]
 
-rna_quant <- ballgown(samples = "data/quant/ballgown/")
+rna_quant <- ballgown(dataDir = "data/quant/ballgown/", samplePattern = "cnss", pData = pheno_data)
 list.files("data/quant/ballgown/")
+
+rna.matrix <- texpr(rna_quant, meas = "all")
+
+write.table(rna.matrix, "data/quant/cns_expresion.tsv", sep = "\t", row.names = FALSE, quote = FALSE)
+
+hist(rna.matrix[,"FPKM.cnssA10A"], breaks = 60000)
+
+low_levels <- rna.matrix[rna.matrix[,"FPKM.cnssA10A"] < 10, "FPKM.cnssA10A"]
+hist(low_levels, breaks = 100)
