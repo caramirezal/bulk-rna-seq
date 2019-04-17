cc6c62cc79150fb0552a01e9bb46c775dd5a230f
caramirezal <cramireza@ciencias.unam.mx>
2019-04-17 15:54:45 -0500

    alignment to mtrna and rrna for quality check

--- scripts/qual_check.sh
+++ scripts/qual_check.sh
@@ -6,7 +6,7 @@ fastq_dir=$(ls data/qual_check/sample_seqs | grep .fastq | sed "s/_R[12]_001.fas
 
 ## Alignment to mitochondria
 for file in $fastq_dir; do
-#	echo "####################################################################################################################"
+	echo "####################################################################################################################"
 
 
 	## run alignment to mtrna
@@ -20,12 +20,12 @@ done
 
 
 for file in $fastq_dir; do
-#	echo "####################################################################################################################"
+	echo "####################################################################################################################"
 
 
-	## run alignment to mtrna
-	echo RUNNING: hisat2 -x data/hisat_indexed_mitocondria/homo_sap_mitocon -1 data/qual_check/sample_seqs/${file}_R1_001.fastq -2 data/qual_check/sample_seqs/${file}_R2_001.fastq -S temporal.sam -p 24 --summary-file data/qual_check/mtrna/${file}.hisat_alignment_summary_file.txt
-	hisat2 -x data/hisat_indexed_mitocondria/homo_sap_mitocon -1 data/qual_check/sample_seqs/${file}_R1_001.fastq -2 data/qual_check/sample_seqs/${file}_R2_001.fastq -S temporal.sam -p 24 --summary-file data/qual_check/mtrna/${file}.hisat_alignment_summary_file.txt
+	## run alignment to rrna
+	echo RUNNING: hisat2 -x data/hisat_indexed_ribosome/homo_sap_ribo -1 data/qual_check/sample_seqs/${file}_R1_001.fastq -2 data/qual_check/sample_seqs/${file}_R2_001.fastq -S temporal.sam -p 24 --summary-file data/qual_check/mtrna/${file}.hisat_alignment_summary_file.txt
+	hisat2 -x data/hisat_indexed_ribosome/homo_sap_ribo -1 data/qual_check/sample_seqs/${file}_R1_001.fastq -2 data/qual_check/sample_seqs/${file}_R2_001.fastq -S temporal.sam -p 24 --summary-file data/qual_check/rrna/${file}.hisat_alignment_summary_file.txt
 	
 	## removing sam file
 	echo removing sam
