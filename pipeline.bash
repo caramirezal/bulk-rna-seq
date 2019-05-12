2aed1848d672e0a32c1fa906cb732688819babbc
caramirezal <cramireza@ciencias.unam.mx>
2019-05-12 18:15:02 -0500

    star pipeline

--- pipeline.bash
+++ pipeline.bash
@@ -1,32 +1,21 @@
 ## RNA quantification pipeline
 
 ## Loading dependencies
-export PATH=$PATH:inst/stringtie/
-export PATH=$PATH:inst/samtools-1.9
+#export PATH=$PATH:inst/stringtie/
+#export PATH=$PATH:inst/samtools-1.9
 
+## path to star
+star_PATH=sc/STAR/bin/MacOSX_x86_64/STAR
 
-####################################################################################################################################################################
-## HISAT2, STRINGTIE pipeline
+## indexing genome
+#./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_genome/ --genomeFastaFiles data/reference_genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbOverhang 100 --sjdbGTFfile  data/annotations/Homo_sapiens.GRCh38.95.gtf --runThreadN 16
 
-## building indexed genome
-## Needs to be run just once
-#hisat2-build -p 24 data/reference_genome/Homo_sapiens.GRCh38.cdna.all.fa data/indexed_ref_genome/homo_sap
+## indexing mitochondria
+#./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_mit --genomeFastaFiles data/mitocondria/homo_sap_mitocondria.fa --runThreadN 16
 
-## alignment
-hisat2 -x data/grch38_snp_tran/genome_snp_tran -1  data/raw_data/A10A_S16_L004_R1_001.fastq.gz -2 data/raw_data/A10A_S16_L004_R2_001.fastq.gz -S data/sam/hisat2/A10A_S16_L004.sam -p 24 --summary-file reports/hisat_alignment_summary_file.txt
+##
+#./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_ribo --genomeFastaFiles data/ribosomal/human_rrna.fa --runThreadN 16
 
-## transform sam to bam files 
-samtools sort -@ 24 -o data/bam/hisat2/A10A_S16_L004.bam data/sam/hisat2/A10A_S16_L004.sam
+## alignment to genome
+./$star_PATH --readFilesIn data/qual_check/sample_seqs/M94_S20_L005_R1_001.fastq data/qual_check/sample_seqs/M94_S20_L005_R2_001.fastq --genomeDir data/star_indexed_ribo/ --runThreadN 16 --outFileNamePrefix data/qual_check/star/
 
-## transcript quantification
-stringtie -p 24 -G data/annotations/Homo_sapiens.GRCh38.95.gtf -o data/quant/stringtie/A10A_S16_L004.gtf -l A10A_S16_L004  data/bam/hisat2/A10A_S16_L004.bam
-
-
-#####################################################################################################################################################################
-## SALMON pipeline
-
-## salmon index building
-salmon index -t data/reference_genome/Homo_sapiens.GRCh38.cdna.all.fa -i data/salmon_indexed_genome/homo_sap
-
-## salmon quantification
-salmon quant -i data/salmon_indexed_genome/homo_sap -l A  -1 data/raw_data/A10A_S16_L004_R1_001.fastq.gz -2 data/raw_data/A10A_S16_L004_R2_001.fastq.gz -p 24 -o data/salmon_quant
