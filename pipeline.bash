65907165639461ef36b1cfe322297d73b9b80129
caramirezal <cramireza@ciencias.unam.mx>
2019-05-27 23:17:32 -0500

    indexing genome

--- pipeline.bash
+++ pipeline.bash
@@ -8,10 +8,11 @@
 star_PATH=sc/STAR/bin/MacOSX_x86_64/STAR
 
 ## indexing genome
-#./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_genome/ --genomeFastaFiles data/reference_genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbOverhang 100 --sjdbGTFfile  data/annotations/Homo_sapiens.GRCh38.95.gtf --runThreadN 16
+echo RUNNING:./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_genome/ --genomeFastaFiles data/reference_genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbOverhang 100 --sjdbGTFfile data/annotations/Homo_sapiens.GRCh38.96.gtf --runThreadN 18
+./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_genome/ --genomeFastaFiles data/reference_genome/Homo_sapiens.GRCh38.dna.primary_assembly.fa --sjdbOverhang 100 --sjdbGTFfile data/annotations/Homo_sapiens.GRCh38.96.gtf --runThreadN 18
 
 ## indexing mitochondria
-./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_mit --genomeFastaFiles data/mitocondria/homo_sap_mitocondria.fa --runThreadN 16 --genomeSAindexNbases 6
+#./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_mit --genomeFastaFiles data/mitocondria/homo_sap_mitocondria.fa --runThreadN 16 --genomeSAindexNbases 6
 
 ## indexing ribosomal unit
 #./$star_PATH --runMode genomeGenerate --genomeDir data/star_indexed_ribo --genomeFastaFiles data/ribosomal/human_rrna.fa --runThreadN 16
