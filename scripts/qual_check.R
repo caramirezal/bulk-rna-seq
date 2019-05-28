7c8d010c6ca06f1f9f6e7427d6844850396076e4
caramirezal <cramireza@ciencias.unam.mx>
2019-05-28 02:02:00 -0500

    alignment qual check with STAR

--- scripts/qual_check.R
+++ scripts/qual_check.R
@@ -6,55 +6,60 @@ library(reshape2)
 
 
 ## extract full paths
-summaries <- list.files("../data/qual_check/", recursive = TRUE)
-summaries <- summaries[grep("hisat_alignment_summary_file.txt", 
-                            summaries)]
-f_path <- paste0("../data/qual_check/", summaries)
-f_path <- f_path[!grepl("R1|R2", f_path)]
-
+summaries <- list.files("../data/qual_check/star", recursive = TRUE)
+f_path <- paste0("../data/qual_check/star/", summaries)
 
 ## extract alignment rates
 extract_align_rate <- function(path){
         content <- readLines(path)
-        content <- content[grep("overall alignment rate", content)]
-        rate <- gsub("% .*", "", content)
+        content <- content[grep("Uniquely mapped reads %", content)]
+        rate <- gsub(".*\t", "", content)
+        rate <- gsub("%", "", rate)
         as.numeric(rate)
 }
 
 ## perform rate extraction
 rates <- sapply(f_path, function(x) extract_align_rate(x))
+rates
 
 ## Tide data
 align_rates <- data.frame("full_paths" = names(rates), 
                           "rates" = rates)
+
 ## extract sample ids from paths
-align_rates <- mutate(align_rates, ids=gsub("_S.*|hisat.*", "", full_paths)) %>%
-                        mutate(ids=gsub(".*/", "", ids))
+align_rates <- mutate(align_rates, ids=gsub("Log.*|-Log.*", "", full_paths)) %>%
+                        mutate(ids=gsub(".*/", "", ids)) %>%
+                            mutate(ids=gsub("-.*", "", ids))
+
 ## extract alignment type
 align_rates <- mutate(align_rates, 
-                      align_type=gsub("../data/qual_check/", "", full_paths)) %>%
-                           mutate(align_type=gsub("/.*", "", align_type))
+                      align_type=gsub("../data/qual_check/star/", "", full_paths)) %>%
+                           mutate(align_type=gsub("Log.*|-Log.*", "", align_type)) %>%
+                           mutate(align_type=gsub(".*-", "", align_type))
+
 
 ## remove unwanted rows and cols
-align_rates.s <- filter(align_rates, align_type!="cdna_n_mtrna") %>%
-        filter(ids !="") %>%
-        mutate(ids=gsub("\\.", "", ids)) %>%
-        select(rates:align_type)
+#align_rates.s <- filter(align_rates, align_type!="cdna_n_mtrna") %>%
+#        filter(ids !="") %>%
+#        mutate(ids=gsub("\\.", "", ids)) %>%
+align_rates.s <- select(align_rates, rates:align_type)
 
 ## reshape dataframe and calculate unaligned sequences
 align_rates.m <- melt(align_rates.s, id=c("ids", "align_type"))
-align_rates.m <- cast(align_rates.m, ids ~ align_type)
-align_rates.m <- mutate(align_rates.m, unalign=100-genome) 
+#align_rates.m
+align_rates.c <- cast(align_rates.m, ids ~ align_type)
+align_rates.c <- mutate(align_rates.c, unalign=100-genome) 
+align_rates.c
 
 ##################################################################################
 ## stacked barplot
 
 ## data processing for stacked barplot of rna alignments
-align_rates.rna <- melt(align_rates.m, id="ids")
+align_rates.rna <- melt(align_rates.c, id="ids")
 align_rates.rna <- filter(align_rates.rna, variable!="genome")
 align_rates.rna$variable <- as.factor(align_rates.rna$variable)
 align_rates.rna$variable <- factor(align_rates.rna$variable, 
-                                 levels=rev(c("cdna", "mtrna",
+                                 levels=rev(c("mtrna",
                                           "rrna", "unalign")))
 
 ## stacked bar plot
@@ -64,11 +69,11 @@ g <- ggplot(data=align_rates.rna, aes(x=ids, y=value, fill=variable)) +
             labs(x="", y="% Alignment") +
             theme(axis.text.x = element_text(face="bold", size = 10),
                   text = element_text(face="bold", size = 16))
-g <- g + geom_hline(yintercept=mean(align_rates.m$genome), 
-                    linetype="dashed", 
-                    color="black", 
-                    size=1)
-g <- g + geom_text(x=2, y=96, label="Alignment to Genome", size=4, color="steelblue")
+#g <- g + geom_hline(yintercept=mean(align_rates.m$genome), 
+#                    linetype="dashed", 
+#                    color="black", 
+#                    size=1)
+#g <- g + geom_text(x=2, y=96, label="Alignment to Genome", size=4, color="steelblue")
 plot(g)
 
 #################################################################################
